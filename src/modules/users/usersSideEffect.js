13f86c00a19ef6898765757a45e2495ee2b557e7
Juan Mendez <juanmndz19@gmail.com>
2020-12-09 18:54:27 -0700

    Fixes

--- src/modules/users/usersSideEffect.js
+++ src/modules/users/usersSideEffect.js
@@ -1,16 +1,16 @@
 import Axios from 'axios';
-import { LOAD_USERS, LOAD_USERS_ERROR, LOAD_USERS_SUCCESS } from './usersReducer';
+import { loadUsers, usersLoaded, usersLoadingError } from './usersReducer';
 
 export const listUsers = () => async (dispatch) => {
-  dispatch({ type: LOAD_USERS });
+  loadUsers()
   try {
     const { data } = await Axios.get('https://jsonplaceholder.typicode.com/users');
-    dispatch({ type: LOAD_USERS_SUCCESS, payload: data });
+    dispatch(usersLoaded(data));
   } catch (error) {
     const message =
       error.response && error.response.data.message
         ? error.response.data.message
         : error.message;
-    dispatch({ type: LOAD_USERS_ERROR, payload: message });
+        usersLoadingError(message)
   }
 }
