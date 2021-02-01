7dae667e0e14cda439ccac9c6967f3c9e9e9c077
Juan Mendez <juanmndz19@gmail.com>
2021-01-31 20:27:59 -0700

    package proxy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,18 +1,5 @@
 import Axios from "axios";
 
-const API_SERVER =  process.env.REACT_APP_ENV ? 'http://localhost:5000' : 'https://grocerygo.herokuapp.com'
-  
-export const getAllBooks = async () => {
-    const response = await fetch(`${API_SERVER}/books`);
-  
-    if (!response.ok) {
-      throw new Error("Something went wrong.");
-    }
-    
-    return response.json();
-  };
-  
-
   export const getOrders = async () => {
     const response = await Axios.get(`/api/order/all`);
     return response.data;
